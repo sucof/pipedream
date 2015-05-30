54b8369213a5e91870b3e2ce0acc6be7543dfdba
CreateRemoteThread <norman.yue@owasp.org>
2015-05-30 17:22:45 +1000

    editor p3
    glorious

--- pipedream.py
+++ pipedream.py
@@ -8,6 +8,7 @@ import socket
 import thread
 import random
 import pickle
+import string
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~ The sun is a wondrous body, like a magnificent father - if only I could be ~
@@ -17,6 +18,7 @@ import pickle
 VERSION="I AM SUN OF HOUSE ARISUN - AND I AM INVINCIBLE"
 
 def prettyPrint(i,d,message):
+  meow = ""
   if d == socketConversation.DIRECTION_FORWARD:
     print " [ message %4d -> len:0x%08x ]" % (i,len(message))
   else:
@@ -25,42 +27,83 @@ def prettyPrint(i,d,message):
   # totalLength = len(message) % 8
   for i in range(0,len(message)):
     if i != 0 and i % 8 == 0:
-      print "]\n [",
+      print " %s ]\n [" % meow,
+      meow = ""
     elif i != 0 and i % 4 == 0:
       print "-",
     print "%02x" %  int(ord(message[i])),
+    if message[i] in string.printable and message[i] != '\r' and message[i] != '\n':
+      meow += message[i]
+    else:
+      meow += "."
   i += 1
   while i % 8 != 0:
     if i != 0 and i % 4 == 0:
       print "-",
     print "..",
+    meow += "."
     i += 1
-  print "]"
+  print " %s ]" % meow
 
 def prettyPrintShort(i,d,message):
+  meow = ""
   if d == socketConversation.DIRECTION_FORWARD:
-    print " [ message %4d -> len:0x%08x ]" % (i,len(message)),
+    print " [ %d -> len:0x%04x ]" % (i,len(message)),
   else:
-    print " [ message %4d <- len:0x%08d ]" % (i,len(message)),
+    print " [ %d <- len:0x%04x ]" % (i,len(message)),
   print " [",
   for i in range(0,len(message)):
     if i != 0 and i % 8 == 0:
       print ""
-    print "%02x " %  int(ord(message[i])),
+    print "%02x" %  int(ord(message[i])),
+    if message[i] in string.printable and message[i] != '\r' and message[i] != '\n':
+      meow += message[i]
+    else:
+      meow += "."
     if i == 7:
       break
-  print "]"
+  while i % 7 != 0:
+    if i != 0 and i % 4 == 0:
+      print "-",
+    print "..",
+    meow += "."
+    i += 1
+  print " %s ]" % meow
+
+def printSelection(m,row,column):
+  meow = ""
+  startI = row * 8
+  maxI = startI + 8
+  print " [",
+  for i in range(startI,maxI):
+    if i >= len(m):
+      print "..",
+      meow += "."
+    else:
+      if i % 8 == column:
+        print "[%02x]" % ord(m[i]),
+      else:
+        print "%02x" % ord(m[i]),
+      if m[i] in string.printable and m[i] != '\r' and m[i] != '\n':
+        meow += m[i]
+      else:
+        meow += "."
+  print " %s ]" % meow,
 
 class conversationEditor:
   def __init__(self,f=None,interactive=True):
     self.selectToken = None
     self.sequence = None
     self.saveFile = None
+    print "---------------------------------------------------------------"
+    self.help()
+    print "---------------------------------------------------------------"
     if f:
       self.sequence = socketConversation(f)
       self.saveFile = f
     if interactive:
       self.editShell()
+    self.changeFlag = False
 
   def printConversation(self):
     if self.sequence is None:
@@ -80,11 +123,56 @@ class conversationEditor:
     print " f: flip selected packet"
     print " d: delete selected packet"
     print " e: edit selected packet"
-    print " s: save sequence"
-  
-  def editPacket(self,tokenNum):
-    (d,m) = self.sequence.fetchMessage(tokenNum)
-    
+    print " s: [file?] save sequence to file (or current file)"
+
+  def editPacketHelp(self):
+    print " q: quit, without save"
+    print " g: quit, with save (not to file)"
+    print " w/s/a/d: control cursor"
+    print " x [byte]: overwrite byte"
+
+  # edit packet
+  def editPacket(self,packet):
+    try:
+      (d,m) = self.sequence.fetchMessage(packet)
+    except:
+      print " [err: could not load %d]" % packet
+      return
+    print "---------------------------------------------------------------"
+    self.editPacketHelp()
+    print "---------------------------------------------------------------"
+    continueFlag = True
+    selectedColumn = 0
+    selectedRow = 0
+    maxRow = len(m) / 8 # max row counted from 0
+    while continueFlag:
+      printSelection(m,selectedRow,selectedColumn)
+      q = raw_input(": ").rstrip().lstrip()
+      commandTokens = q.split(" ")
+      c = commandTokens[0]
+      if c in ("q","quit"):
+        continueFlag = False
+      elif c in ("h","help"):
+        self.editPacketHelp()
+      elif c in ("w","s"):
+        if c == "w" and selectedRow > 0:
+          selectedRow -= 1
+        elif c == "s" and selectedRow < maxRow:
+          selectedRow += 1
+        selectedColumn = 0
+      elif c in ("a","d"):
+        if c == "a" and selectedColumn > 0:
+          selectedColumn -= 1
+        elif c == "d" and selectedColumn < 7 and (selectedRow * 8 + selectedColumn < len(m) - 1): # 8 width, so 6->7 is the last 'd' command
+          selectedColumn += 1
+      elif c == "g":
+        self.sequence.saveMessage(packet,(d,m))
+        self.changeFlag = True
+        continueFlag = False
+      elif c == "x" and len(commandTokens) == 2 and len(commandTokens[1]) == 2:
+        tempM = bytearray(m)
+        tempM[selectedRow * 8 + selectedColumn] = chr( int(commandTokens[1],16) )
+        m = str(tempM)
 
   def editShell(self):
     continueFlag = True
@@ -115,22 +203,31 @@ class conversationEditor:
             except:
               print " [err: could not fetch message %d]" % i
       elif c in ("s","select") and len(commandTokens) == 2:
-        self.selectToken = int(commandTokens[1])
-        if self.selectToken > len(self.sequence.messages):
+        try:
+          self.selectToken = int(commandTokens[1])
+          if self.selectToken > len(self.sequence.messages):
+            self.selectToken = None
+        except:
           self.selectToken = None
+      elif c in ("s","select") and len(commandTokens) == 1:
+        self.selectToken = None
       elif c in ("f","flip"):
         try:
-          (d,m) = self.selection.messages[self.selectToken])
-          self.selection.messages[self.selectToken] = (2 - d + 1, m)
+          (d,m) = self.sequence.messages[self.selectToken]
+          self.sequence.messages[self.selectToken] = (2 - d + 1, m)
+          self.changeFlag = True
         except:
-          print " [err: could not fetch message %d] % self.selectToken
+          print " [err: could not fetch message %d]" % self.selectToken
       elif c in ("d","del","delete","rm"):
         try:
           del self.sequence.messages[self.selectToken]
           if self.selectToken > len(self.sequence.messages):
             self.selectToken = None
+          self.changeFlag = True
         except:
-          print " [err: could not delete message %d] % self.selectToken
+          print " [err: could not delete message %d]" % self.selectToken
+        if self.selectToken > len(self.sequence.messages) - 1:
+          self.selectToken = None
       elif c in ("l","load") and len(commandTokens) == 2:
         self.sequence = socketConversation(commandTokens[1])
         self.saveFile = commandTokens[1]
@@ -139,6 +236,7 @@ class conversationEditor:
           self.sequence.saveToFile(commandTokens[1])
         elif self.saveFile is not None:
           self.sequence.saveToFile(self.saveFile)
+        self.changeFlag = False
       elif c in ("e","edit") and self.selectToken is not None:
         self.editPacket(self.selectToken)
       elif c in ("h","help"):
@@ -181,6 +279,10 @@ class socketConversation:
 
   def fetchMessage(self,i):
     return self.messages[i]
+
+  def saveMessage(self,i,message):
+    (d,m) = message
+    self.messages[i] = (d,m)
 
 class replayClient:
   def __init__(self,outHost,outPort,socketConv,sslreq=False):
